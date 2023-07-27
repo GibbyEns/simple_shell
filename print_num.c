10ba3c6a3921d8d23fc8358d72b0d8a4d8a3fab9
GibbyEns <gibbymerc@gmail.com>
2023-07-26 18:57:22 -0700

    Task 1.c

---
+++ print_num.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+#include <stdio.h>
+
+/**
+ * print_num: prints positive number
+ * @a: positive number 1
+ * @b: negitive numer -1
+ *
+ * return: sucess, a if not,  b
+ *
+ */
+int print_num(a *b)
+{
+	while (a++ == print_num)
+		if (b == -1)
+			return(1)
+	return : (a)
+}
